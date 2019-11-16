7cdf040dbef1821a14eae9f89199e6795bb8457f
BombadilToma <newlocal2@gmail.com>
2019-11-16 10:53:56 +0200

    first commit

---
+++ Termometru_Cluj.py
@@ -0,0 +1,23 @@
+# get_temp
+# set_temp
+# cesius
+# fahrenheit
+# class
+
+class temperature_celsius:
+    def __init__(self, celsius, fahrenheit):
+        self.celsius = celsius
+        self.fahrenheit = fahrenheit
+
+    def get_temp(self):
+        print(self.celsius)
+        print(self.fahrenheit)
+
+    def set_temp():
+
+    def convert_in_celsius(self, fahrenheit):
+        self.celsius = (fahrenheit-32)/1.8
+        return self_celsius
+
+    def convert_in_fahrenheit(selfi, celsius):
+        self.fahrenheit = 
